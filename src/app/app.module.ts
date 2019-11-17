903cb31270b50b71b03e0207273034aabd908591
unknown <mariazarapuz@gmail.com>
2019-11-17 17:45:33 +0100

    creacion de componenetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPersonComponent } from './components/add-person/add-person.component';
+import { EditPersonComponent } from './components/edit-person/edit-person.component';
+import { PersonsListComponent } from './components/persons-list/persons-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPersonComponent,
+    EditPersonComponent,
+    PersonsListComponent
   ],
   imports: [
     BrowserModule,
